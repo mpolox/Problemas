efb747db67459163a9b6c29d67283df4fdbdbb25
Marcopolo Ramos <mpolox@gmail.com>
2022-01-28 23:15:13 -0600

    updated

---
+++ Problemas/Models/Problema.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Problemas.Models
+{
+    public class Problema
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public string More { get; set; }
+    }
+}
+    
