f71992955ec9bc4dc054671194cdf62c69204f9d
Marcopolo Ramos <mpolox@gmail.com>
2022-01-28 21:24:24 -0600

    first draft

---
+++ Problemas/Controllers/ProblemasController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Problemas.Controllers
+{
+    [ApiController]
+    [Route("api/Problemas")]
+    public class ProblemasController
+    {
+        [HttpGet]
+        public int Get()
+        {
+            return 3;
+        }
+    }
+}
